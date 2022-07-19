1ad36f9e1e6e7b183b75f03fdebc33a35b2b4d2c
noriakikano <109402633+noriakikano@users.noreply.github.com>
2022-07-19 15:42:59 +0900

    完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
-import React, { StrictMode } from "react";
+// eslint react-hooks/exhaustive-deps:off
+
+import React, { StrictMode, useEffect, useState } from "react";
 import ReactDom from "react-dom";
 import ColufulMessage from "./componets/ColourfulMessage";
 
 const App = () => {
-  const onCllickButton = () => {
-    return alert();
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceshowFlag] = useState(false);
+
+  // const onCllickButton = () => {
+  //   return alert();
+  // };
+  const onCllickCuntup = () => {
+    setNum(num + 1);
   };
+
+  const onClickFaceShowFlag = () => {
+    setFaceshowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceshowFlag(true);
+    } else {
+      faceShowFlag && setFaceshowFlag(false);
+    }
+  }, [num]);
 
   return (
     <>
       <h1 style={{ color: "red" }}>aaんにちは</h1>
       <ColufulMessage color="blue">お元気ですか？</ColufulMessage>
       <ColufulMessage color="pink">　チルドレン </ColufulMessage>
-      <button onClick={onCllickButton}>送信</button>
+      <button onClick={onCllickCuntup}>countup</button>
+      <p>{num}</p>
+      <br />
+      <button onClick={onClickFaceShowFlag}> on/off</button>
+      {faceShowFlag && <p>^^</p>}
     </>
   );
 };
